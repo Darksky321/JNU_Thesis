c0d80fc124aed305b4a1d5d78bddeab0a730694d
Darksky321 <348068854@qq.com>
2015-03-04 18:25:06 +0800

    增加单元测试模块

---
+++ Thesis/src/com/jnu/thesis/test/TestCase.java
@@ -0,0 +1,12 @@
+package com.jnu.thesis.test;
+
+import android.test.AndroidTestCase;
+import android.util.Log;
+
+public class TestCase extends AndroidTestCase {
+	private static final String TAG = "MyTest";
+
+	public static void test() {
+		Log.i(TAG, "test");
+	}
+}
